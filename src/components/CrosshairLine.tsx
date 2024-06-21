177cf9e054dba1437db12bbe8dd35cc8fa0d3901
Carlos Henryck <ryck302@gmail.com>
2024-06-21 16:12:27 -0300

    refactor: create component for lines

--- src/components/CrosshairLine.tsx
+++ src/components/CrosshairLine.tsx
@@ -1,4 +1,10 @@
+import { CSSProperties } from "react";
 import { useCrosshairStore } from "../stores/CrosshairStore";
+import { getBackgroundColor } from "../utils/colorsFn";
+import {
+  showInnerVerticalLines,
+  showOuterVerticalLines,
+} from "../utils/showVerticalLines";
 
 interface CrosshairLineProps {
   position: "top" | "right" | "bottom" | "left";
@@ -7,40 +13,130 @@ interface CrosshairLineProps {
 
 export default function CrosshairLine({ position, type }: CrosshairLineProps) {
   const crosshairConfig = useCrosshairStore((store) => store.crosshairConfig);
+  const {
+    innerLength,
+    innerThickness,
+    innerOffset,
+    innerAlpha,
+    innerVerticalEnabled,
+    innerVerticalLength,
+    outerLength,
+    outerThickness,
+    outerOffset,
+    outerAlpha,
+    outerVerticalEnabled,
+    outerVerticalLength,
+    outlinesEnabled,
+    outlinesAlpha,
+    outlinesThickness,
+  } = crosshairConfig;
 
-  const innerStyles = {
+  const innerStyles: { [key: string]: CSSProperties } = {
     left: {
-      width: `${crosshairConfig.innerLength}px`,
-      height: `${crosshairConfig.innerThickness}px`,
-      transform: `translateX(calc(-50% - ${crosshairConfig.innerOffset}px))`,
+      width: `${innerLength}px`,
+      height: `${innerThickness}px`,
+      transform: `translateX(calc(-50% - ${innerOffset}px))`,
+      backgroundColor: `#${getBackgroundColor(+innerAlpha * 100)}`,
+      boxShadow: `${
+        outlinesEnabled
+          ? `rgba(0, 0, 0, ${outlinesAlpha}) 0px 0px 0px ${outlinesThickness}px`
+          : ""
+      }`,
+      display: `${innerLength !== "0" ? "block" : "none"}`,
     },
     right: {
-      width: `${crosshairConfig.innerLength}px`,
-      height: `${crosshairConfig.innerThickness}px`,
-      transform: `translateX(calc(50% + ${crosshairConfig.innerOffset}px))`,
+      width: `${innerLength}px`,
+      height: `${innerThickness}px`,
+      transform: `translateX(calc(50% + ${innerOffset}px))`,
+      backgroundColor: `#${getBackgroundColor(+innerAlpha * 100)}`,
+      boxShadow: `${
+        outlinesEnabled
+          ? `rgba(0, 0, 0, ${outlinesAlpha}) 0px 0px 0px ${outlinesThickness}px`
+          : ""
+      }`,
+      display: `${innerLength !== "0" ? "block" : "none"}`,
     },
     top: {
-      transform: `translateY(calc(-50% - ${crosshairConfig.innerOffset}px))`,
+      height: `${innerVerticalEnabled ? innerVerticalLength : innerLength}px`,
+      width: `${innerThickness}px`,
+      transform: `translateY(calc(-50% - ${innerOffset}px))`,
+      backgroundColor: `#${getBackgroundColor(+innerAlpha * 100)}`,
+      boxShadow: `${
+        outlinesEnabled
+          ? `rgba(0, 0, 0, ${outlinesAlpha}) 0px 0px 0px ${outlinesThickness}px`
+          : ""
+      }`,
+      display: `${showInnerVerticalLines() ? "block" : "none"}`,
     },
     bottom: {
-      transform: `translateY(calc(50% + ${crosshairConfig.innerOffset}px))`,
+      height: `${innerVerticalEnabled ? innerVerticalLength : innerLength}px`,
+      width: `${innerThickness}px`,
+      transform: `translateY(calc(50% + ${innerOffset}px))`,
+      backgroundColor: `#${getBackgroundColor(+innerAlpha * 100)}`,
+      boxShadow: `${
+        outlinesEnabled
+          ? `rgba(0, 0, 0, ${outlinesAlpha}) 0px 0px 0px ${outlinesThickness}px`
+          : ""
+      }`,
+      display: `${showInnerVerticalLines() ? "block" : "none"}`,
     },
   };
 
-  const outerStyles = {
+  const outerStyles: { [key: string]: CSSProperties } = {
     left: {
-      transform: `translateX(calc(-50% - ${crosshairConfig.outerOffset}px))`,
+      width: `${outerLength}px`,
+      height: `${outerThickness}px`,
+      transform: `translateX(calc(-50% - ${outerOffset}px))`,
+      backgroundColor: `#${getBackgroundColor(+outerAlpha * 100)}`,
+      boxShadow: `${
+        outlinesEnabled
+          ? `rgba(0, 0, 0, ${outlinesAlpha}) 0px 0px 0px ${outlinesThickness}px`
+          : ""
+      }`,
+      display: `${outerLength !== "0" ? "block" : "none"}`,
     },
     right: {
-      transform: `translateX(calc(50% + ${crosshairConfig.outerOffset}px))`,
+      width: `${outerLength}px`,
+      height: `${outerThickness}px`,
+      transform: `translateX(calc(50% + ${outerOffset}px))`,
+      backgroundColor: `#${getBackgroundColor(+outerAlpha * 100)}`,
+      boxShadow: `${
+        outlinesEnabled
+          ? `rgba(0, 0, 0, ${outlinesAlpha}) 0px 0px 0px ${outlinesThickness}px`
+          : ""
+      }`,
+      display: `${outerLength !== "0" ? "block" : "none"}`,
     },
     top: {
-      transform: `translateY(calc(-50% - ${crosshairConfig.outerOffset}px))`,
+      height: `${outerVerticalEnabled ? outerVerticalLength : outerLength}px`,
+      width: `${outerThickness}px`,
+      transform: `translateY(calc(-50% - ${outerOffset}px))`,
+      backgroundColor: `#${getBackgroundColor(+outerAlpha * 100)}`,
+      boxShadow: `${
+        outlinesEnabled
+          ? `rgba(0, 0, 0, ${outlinesAlpha}) 0px 0px 0px ${outlinesThickness}px`
+          : ""
+      }`,
+      display: `${showOuterVerticalLines() ? "block" : "none"}`,
     },
     bottom: {
-      transform: `translateY(calc(50% + ${crosshairConfig.outerOffset}px))`,
+      height: `${outerVerticalEnabled ? outerVerticalLength : outerLength}px`,
+      width: `${outerThickness}px`,
+      transform: `translateY(calc(50% + ${outerOffset}px))`,
+      backgroundColor: `#${getBackgroundColor(+outerAlpha * 100)}`,
+      boxShadow: `${
+        outlinesEnabled
+          ? `rgba(0, 0, 0, ${outlinesAlpha}) 0px 0px 0px ${outlinesThickness}px`
+          : ""
+      }`,
+      display: `${showOuterVerticalLines() ? "block" : "none"}`,
     },
   };
 
-  return <div className="absolute box-content transition-transform"></div>;
+  return (
+    <div
+      className="absolute box-content transition-transform"
+      style={type === "inner" ? innerStyles[position] : outerStyles[position]}
+    />
+  );
 }
--- src/components/CrosshairPreview.tsx
+++ src/components/CrosshairPreview.tsx
@@ -1,9 +1,6 @@
 import { useCrosshairStore } from "../stores/CrosshairStore";
 import { getBackgroundColor } from "../utils/colorsFn";
-import {
-  showInnerVerticalLines,
-  showOuterVerticalLines,
-} from "../utils/showVerticalLines";
+import CrosshairLine from "./CrosshairLine";
 
 export default function CrosshairPreview() {
   const crosshairConfig = useCrosshairStore((store) => store.crosshairConfig);
@@ -12,170 +9,18 @@ export default function CrosshairPreview() {
     <div className="size-28 bg-slate-600 relative">
       {crosshairConfig.innerEnabled && (
         <div className="size-full flex items-center justify-center absolute">
-          {crosshairConfig.innerLength !== "0" && (
-            <>
-              <div
-                className="absolute box-content transition-transform"
-                style={{
-                  width: `${crosshairConfig.innerLength}px`,
-                  height: `${crosshairConfig.innerThickness}px`,
-                  backgroundColor: `#${getBackgroundColor(
-                    +crosshairConfig.innerAlpha * 100
-                  )}`,
-                  transform: `translateX(calc(-50% - ${crosshairConfig.innerOffset}px))`,
-                  boxShadow: `${
-                    crosshairConfig.outlinesEnabled
-                      ? `rgba(0, 0, 0, ${crosshairConfig.outlinesAlpha}) 0px 0px 0px ${crosshairConfig.outlinesThickness}px`
-                      : ""
-                  }`,
-                }}
-              ></div>
-              <div
-                className="absolute box-content transition-transform"
-                style={{
-                  width: `${crosshairConfig.innerLength}px`,
-                  height: `${crosshairConfig.innerThickness}px`,
-                  backgroundColor: `#${getBackgroundColor(
-                    +crosshairConfig.innerAlpha * 100
-                  )}`,
-                  transform: `translateX(calc(50% + ${crosshairConfig.innerOffset}px))`,
-                  boxShadow: `${
-                    crosshairConfig.outlinesEnabled
-                      ? `rgba(0, 0, 0, ${crosshairConfig.outlinesAlpha}) 0px 0px 0px ${crosshairConfig.outlinesThickness}px`
-                      : ""
-                  }`,
-                }}
-              ></div>
-            </>
-          )}
-          {showInnerVerticalLines() && (
-            <>
-              <div
-                className="absolute box-content transition-transform"
-                style={{
-                  height: `${
-                    crosshairConfig.innerVerticalEnabled
-                      ? crosshairConfig.innerVerticalLength
-                      : crosshairConfig.innerLength
-                  }px`,
-                  width: `${crosshairConfig.innerThickness}px`,
-                  backgroundColor: `#${getBackgroundColor(
-                    +crosshairConfig.innerAlpha * 100
-                  )}`,
-                  transform: `translateY(calc(-50% - ${crosshairConfig.innerOffset}px))`,
-                  boxShadow: `${
-                    crosshairConfig.outlinesEnabled
-                      ? `rgba(0, 0, 0, ${crosshairConfig.outlinesAlpha}) 0px 0px 0px ${crosshairConfig.outlinesThickness}px`
-                      : ""
-                  }`,
-                }}
-              ></div>
-              <div
-                className="absolute box-content transition-transform"
-                style={{
-                  height: `${
-                    crosshairConfig.innerVerticalEnabled
-                      ? crosshairConfig.innerVerticalLength
-                      : crosshairConfig.innerLength
-                  }px`,
-                  width: `${crosshairConfig.innerThickness}px`,
-                  backgroundColor: `#${getBackgroundColor(
-                    +crosshairConfig.innerAlpha * 100
-                  )}`,
-                  transform: `translateY(calc(50% + ${crosshairConfig.innerOffset}px))`,
-                  boxShadow: `${
-                    crosshairConfig.outlinesEnabled
-                      ? `rgba(0, 0, 0, ${crosshairConfig.outlinesAlpha}) 0px 0px 0px ${crosshairConfig.outlinesThickness}px`
-                      : ""
-                  }`,
-                }}
-              ></div>
-            </>
-          )}
+          <CrosshairLine position="left" type="inner" />
+          <CrosshairLine position="right" type="inner" />
+          <CrosshairLine position="top" type="inner" />
+          <CrosshairLine position="bottom" type="inner" />
         </div>
       )}
       {crosshairConfig.outerEnabled && (
         <div className="size-full flex items-center justify-center absolute">
-          {crosshairConfig.outerLength !== "0" && (
-            <>
-              <div
-                className="absolute box-content transition-transform"
-                style={{
-                  width: `${crosshairConfig.outerLength}px`,
-                  height: `${crosshairConfig.outerThickness}px`,
-                  backgroundColor: `#${getBackgroundColor(
-                    +crosshairConfig.outerAlpha * 100
-                  )}`,
-                  transform: `translateX(calc(-50% - ${crosshairConfig.outerOffset}px))`,
-                  boxShadow: `${
-                    crosshairConfig.outlinesEnabled
-                      ? `rgba(0, 0, 0, ${crosshairConfig.outlinesAlpha}) 0px 0px 0px ${crosshairConfig.outlinesThickness}px`
-                      : ""
-                  }`,
-                }}
-              ></div>
-              <div
-                className="absolute box-content transition-transform"
-                style={{
-                  width: `${crosshairConfig.outerLength}px`,
-                  height: `${crosshairConfig.outerThickness}px`,
-                  backgroundColor: `#${getBackgroundColor(
-                    +crosshairConfig.outerAlpha * 100
-                  )}`,
-                  transform: `translateX(calc(50% + ${crosshairConfig.outerOffset}px))`,
-                  boxShadow: `${
-                    crosshairConfig.outlinesEnabled
-                      ? `rgba(0, 0, 0, ${crosshairConfig.outlinesAlpha}) 0px 0px 0px ${crosshairConfig.outlinesThickness}px`
-                      : ""
-                  }`,
-                }}
-              ></div>
-            </>
-          )}
-          {showOuterVerticalLines() && (
-            <>
-              <div
-                className="absolute box-content transition-transform"
-                style={{
-                  height: `${
-                    crosshairConfig.outerVerticalEnabled
-                      ? crosshairConfig.outerVerticalLength
-                      : crosshairConfig.outerLength
-                  }px`,
-                  width: `${crosshairConfig.outerThickness}px`,
-                  backgroundColor: `#${getBackgroundColor(
-                    +crosshairConfig.outerAlpha * 100
-                  )}`,
-                  transform: `translateY(calc(-50% - ${crosshairConfig.outerOffset}px))`,
-                  boxShadow: `${
-                    crosshairConfig.outlinesEnabled
-                      ? `rgba(0, 0, 0, ${crosshairConfig.outlinesAlpha}) 0px 0px 0px ${crosshairConfig.outlinesThickness}px`
-                      : ""
-                  }`,
-                }}
-              ></div>
-              <div
-                className="absolute box-content transition-transform"
-                style={{
-                  height: `${
-                    crosshairConfig.outerVerticalEnabled
-                      ? crosshairConfig.outerVerticalLength
-                      : crosshairConfig.outerLength
-                  }px`,
-                  width: `${crosshairConfig.outerThickness}px`,
-                  backgroundColor: `#${getBackgroundColor(
-                    +crosshairConfig.outerAlpha * 100
-                  )}`,
-                  transform: `translateY(calc(50% + ${crosshairConfig.outerOffset}px))`,
-                  boxShadow: `${
-                    crosshairConfig.outlinesEnabled
-                      ? `rgba(0, 0, 0, ${crosshairConfig.outlinesAlpha}) 0px 0px 0px ${crosshairConfig.outlinesThickness}px`
-                      : ""
-                  }`,
-                }}
-              ></div>
-            </>
-          )}
+          <CrosshairLine position="left" type="outer" />
+          <CrosshairLine position="right" type="outer" />
+          <CrosshairLine position="top" type="outer" />
+          <CrosshairLine position="bottom" type="outer" />
         </div>
       )}
       {crosshairConfig.dotEnabled && (
